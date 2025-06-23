832c911b81255c9a762e4caa656a48ddcc1519bf
rev-se <revathicutyou@gmail.com>
2025-06-23 17:19:14 +0800

    getting things set-up

---
+++ restaurant-finder/src/lib/apiConfig.ts
@@ -0,0 +1,12 @@
+export const API_CONFIG = {
+  normantin: {
+    url: import.meta.env.VITE_NORMANTIN_API_URL,
+    key: import.meta.env.VITE_NORMANTIN_API_KEY,
+  },
+  overpass: {
+    url: import.meta.env.VITE_OVERPASS_API_URL,
+  },
+  userDb: {
+    url: import.meta.env.VITE_USER_DB_URL,
+  },
+}; 
